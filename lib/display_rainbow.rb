d1538cfda993b35d69d734314f33936686931adf
Boruch Zidell <philomath613@gmail.com>
2019-02-22 18:50:47 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,13 +1,13 @@
 rainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 
 def display_rainbow(color_array)
-  p "R: #{color_array[0]},"
-  p "O: #{color_array[1]},"
-  p "Y: #{color_array[2]},"
-  p "G: #{color_array[3]},"
-  p "B: #{color_array[4]},"
-  p "I: #{color_array[5]},"
-  p "V: #{color_array[6]}"
+  print "R: #{color_array[0]},"
+  print "O: #{color_array[1]},"
+  print "Y: #{color_array[2]},"
+  print "G: #{color_array[3]},"
+  print "B: #{color_array[4]},"
+  print "I: #{color_array[5]},"
+  print "V: #{color_array[6]}"
 end
 
 display_rainbow(rainbow)
